b2984e268b6a5a9fec8b36148f77c50475fc58b6
Helson <helsonxiao@gmail.com>
2023-05-25 11:58:09 +0800

    feat: remove recommended activities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Feed Blacklist
 // @namespace    https://github.com/helsonxiao
-// @version      0.1
+// @version      0.2
 // @description  GitHub Feed 黑名单插件
 // @author       helsonxiao
 // @match        https://github.com/
@@ -11,39 +11,88 @@
 (function () {
   "use strict";
 
-  const targetList = JSON.parse(localStorage.getItem("feed-blacklist") || "[]");
+  const targetList = JSON.parse(
+    localStorage.getItem("feed-blacklist") || '[""]'
+  );
 
-  function removeTarget() {
-    const spans = document.querySelectorAll("span.user-following-container");
+  function removeTargets() {
+    const followingContainers = document.querySelectorAll(
+      "span.user-following-container"
+    );
+    const feedItems = document.querySelectorAll(".js-feed-item-component");
     for (let i = 0; i < targetList.length; i++) {
       const target = targetList[i];
-      for (const span of spans) {
-        const form = span.querySelector(
-          'form[action="/users/follow?target=' + target + '"]'
+
+      // remove recommended users
+      for (const followingContainer of followingContainers) {
+        const form = followingContainer.querySelector(
+          `form[action="/users/follow?target=${target}"]`
         );
         if (form) {
-          const parent = span.closest(".col-12.col-xl-6");
-          if (parent) {
-            parent.remove();
-          }
+          const parent = followingContainer.closest(".col-12.col-xl-6");
+          parent.remove();
+          console.info(
+            "[GitHub Feed Blacklist] target has been removed:",
+            target,
+            parent
+          );
         }
       }
+
+      // remove recommended activities
+      feedItems.forEach((feedItem) => {
+        const anchor = feedItem.querySelector(`a[href="/${target}"]`);
+        if (anchor) {
+          feedItem.remove();
+          console.info(
+            "[GitHub Feed Blacklist] target has been removed:",
+            target,
+            feedItem
+          );
+        }
+      });
     }
   }
 
-  window.onload = function () {
-    removeTarget();
-  };
-  function throttle(func, delay) {
-    let timer = null;
-    return function () {
-      if (!timer) {
-        timer = setTimeout(() => {
-          func.apply(this, arguments);
-          timer = null;
-        }, delay);
-      }
-    };
+  function observeFeedFrame(feedFrame) {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length > 0) {
+          removeTargets();
+        }
+      });
+    });
+    observer.observe(feedFrame, {
+      childList: true,
+    });
   }
-  window.addEventListener("scroll", throttle(removeTarget, 1000));
+
+  window.onload = function () {
+    const feedContainer = document
+      .querySelector("#panel-2")
+      .querySelector('[data-target="feed-container.content"]');
+    const initObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length > 0) {
+          const feedFrame = feedContainer.querySelector("#conduit-feed-frame");
+          console.log(
+            "[GitHub Feed Blacklist] initObserver found feedFrame:",
+            feedFrame
+          );
+          if (feedFrame) {
+            initObserver.disconnect();
+            removeTargets();
+            observeFeedFrame(feedFrame);
+          }
+        }
+      });
+    });
+    console.debug(
+      "[GitHub Feed Blacklist] feedContainer:",
+      feedContainer.cloneNode(true)
+    );
+    initObserver.observe(feedContainer, {
+      childList: true,
+    });
+  };
 })();
